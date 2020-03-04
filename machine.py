c2319fa68c3b620fb108ac6833e829834b074259
nullptr <3621629+0x0L@users.noreply.github.com>
2020-03-04 20:16:37 +0100

    memory

--- machine.py
+++ machine.py
@@ -1,6 +1,16 @@
+import struct
+
+
 class Machine:
-    def __init__(self):
+    def __init__(self, memsize=65536):
         self.items = []
+        self.memory = bytearray(memsize)
+
+    def load(self, addr):
+        return struct.unpack('<d', self.memory[addr:addr+8])[0]
+
+    def store(self, addr, val):
+        self.memory[addr:addr+8] = struct.pack('<d', val)
 
     def push(self, item):
         self.items.append(item)
@@ -21,22 +31,38 @@ class Machine:
                 right = self.pop()
                 left = self.pop()
                 self.push(left * right)
+            elif op == 'load':
+                addr = self.pop()
+                self.push(self.load(addr))
+            elif op == 'store':
+                val = self.pop()
+                addr = self.pop()
+                self.store(addr, val)
             else:
                 raise RuntimeError(f'Bad op {op}')
 
 
 def example():
+    x_addr = 0
+    v_addr = 8
+
     code = [
-        ('const', 2),
-        ('const', 3),
+        ('const', x_addr),
+        ('const', x_addr),
+        ('load',),
+        ('const', v_addr),
+        ('load',),
         ('const', 0.1),
-        ('mul', ),
-        ('add', ),
+        ('mul',),
+        ('add',),
+        ('store',)
     ]
 
     m = Machine()
+    m.store(x_addr, 2.0)
+    m.store(v_addr, 3.0)
     m.execute(code)
-    print('Result:', m.pop())
+    print('Result:', m.load(x_addr))
 
 
 if __name__ == '__main__':
